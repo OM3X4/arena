15b90f024cd1c4f3fd8eb6a0101eaf069b33ec45
Omar Emad <om3x4e@gmail.com>
2026-02-05 14:54:37 +0200

    Added Game struct to handle game and state Added Game Result struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,88 @@ use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use std::process::{Child, ChildStdin, ChildStdout, Command, Stdio};
 
+use queenfish::board::{Board, Move, Turn};
+
 struct Engine {
     path: String,
     name: String,
     child_process: Child,
     stdin: ChildStdin,
     stdout: BufReader<ChildStdout>,
-}
+} //
 
 struct Game {
     white: Engine,
     black: Engine,
     moves_list: Vec<String>,
+    board: Board,
 }
 
+#[derive(Debug)]
+struct GameResult {
+    white: String,
+    black: String,
+    moves_list: Vec<String>,
+    white_won: bool,
+}
+
+impl Game {
+    pub fn new(white: Engine, black: Engine) -> Self {
+        Game {
+            white,
+            black,
+            moves_list: Vec::new(),
+            board: Board::new(),
+        }
+    } //
+
+    pub fn play(&mut self) -> GameResult {
+        loop {
+            let valid_moves = self.board.generate_moves();
+            if valid_moves.is_empty() {
+                match self.board.turn {
+                    Turn::WHITE => println!("{} wins as black", self.black.name),
+                    Turn::BLACK => println!("{} wins as white", self.white.name),
+                }
+                return GameResult {
+                    white: self.white.name.clone(),
+                    black: self.black.name.clone(),
+                    moves_list: self.moves_list.clone(),
+                    white_won: self.board.turn == Turn::BLACK,
+                }
+            }
+            let engine = match self.board.turn {
+                Turn::WHITE => &mut self.white,
+                Turn::BLACK => &mut self.black,
+            };
+            if self.moves_list.is_empty() {
+                engine.send_command(format!("position startpos\n").as_str());
+            } else {
+                engine.send_command(
+                    format!("position startpos moves {}\n", self.moves_list.join(" ")).as_str(),
+                );
+            }
+            engine.send_command("go movetime 10\n");
+
+            loop {
+                if let Some(line) = engine.read_line() {
+                    if line.starts_with("bestmove") {
+                        let best_move = line.split_whitespace().nth(1).unwrap();
+                        self.moves_list.push(best_move.to_string());
+                        self.board.make_move(Move::from_uci(best_move, &self.board));
+                        break;
+                    } else {
+                        // println!("{}", line);
+                    }
+                }
+            }
+        };
+
+    } //
+} //
+
 impl Engine {
-    fn new(path: &str, name: &str) -> Self {
+    pub fn new(path: &str, name: &str) -> Self {
         let path = Path::new(path);
 
         if !path.exists() {
@@ -54,10 +120,10 @@ impl Engine {
             .write_all("uci\n".as_bytes())
             .expect("Failed to write 'uci' to engine stdin");
 
-        let mut is_uci_ok = false;
+        let is_uci_ok;
         loop {
             let mut line = String::new();
-            stdout.read_line(&mut line);
+            let _ = stdout.read_line(&mut line);
             if line.starts_with("uciok") {
                 is_uci_ok = true;
                 break;
@@ -76,32 +142,25 @@ impl Engine {
         }
     } //
 
-    fn send_command(&mut self, command: &str) {
+    pub fn send_command(&mut self, command: &str) {
         self.stdin
             .write_all(command.as_bytes())
             .expect("Failed to write command to engine stdin");
         self.stdin.flush().unwrap();
-    }
+    } //
 
     pub fn read_line(&mut self) -> Option<String> {
         let mut line = String::new();
         self.stdout.read_line(&mut line).ok()?;
         if line.is_empty() { None } else { Some(line) }
-    }
-} //
-
-struct Tournament {
-    engines: Vec<Engine>,
-    name: String,
-    rounds: u32,
-    time_per_move: u32,
+    } //
 } //
 
 mod test {
     use super::*;
-    use queenfish::board::{Board, Move, Turn};
     use queenfish::board::bishop_magic::init_bishop_magics;
     use queenfish::board::rook_magic::init_rook_magics;
+    use queenfish::board::{Board, Move, Turn};
 
     #[test]
     fn it_works() {
@@ -118,49 +177,8 @@ mod test {
             "Stockfish",
         );
 
-        let mut board = Board::new();
-        let mut game = Game {
-            white: engine,
-            black: engine2,
-            moves_list: Vec::new(),
-        };
-        loop {
-            let valid_moves = board.generate_moves();
-            if valid_moves.is_empty() {
-                match board.turn {
-                    Turn::WHITE => println!("{} wins as black" , game.black.name),
-                    Turn::BLACK => println!("{} wins as white" , game.white.name),
-                }
-                println!("PGN: {}", game.moves_list.join(" "));
-                break;
-            }
-            let engine = match board.turn {
-                Turn::WHITE => &mut game.white,
-                Turn::BLACK => &mut game.black,
-            };
-            if game.moves_list.is_empty() {
-                engine
-                    .send_command(format!("position startpos\n").as_str());
-            } else {
-                engine.send_command(
-                    format!("position startpos moves {}\n", game.moves_list.join(" ")).as_str(),
-                );
-            }
-            engine.send_command("go movetime 10\n");
+        let mut game = Game::new(engine , engine2);
+        dbg!(game.play());
 
-            loop {
-                if let Some(line) = engine.read_line() {
-                    if line.starts_with("bestmove") {
-                        let best_move = line.split_whitespace().nth(1).unwrap();
-                        game.moves_list.push(best_move.to_string());
-                        board.make_move(Move::from_uci(best_move, &board));
-                        println!("Played {} , fen {}", best_move, board.to_fen());
-                        break;
-                    } else {
-                        // println!("{}", line);
-                    }
-                }
-            }
-        }
     }
 } //
